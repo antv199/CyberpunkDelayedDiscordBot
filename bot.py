850b41ca4f5c8d017ae4f92044652bb13a7c8cfd
Anthony <batmanplayer123@gmail.com>
2020-11-21 23:40:34 +0200

    test

--- bot.py
+++ bot.py
@@ -15,7 +15,12 @@ def getStatus():
         if test:
             driver=webdriver.Edge()
         else:
-            driver=webdriver.Chrome(executable_path="/app/.apt/usr/bin/google-chrome")
+            chrome_options = webdriver.ChromeOptions()
+            chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
+            chrome_options.add_argument("--headless")
+            chrome_options.add_argument("--disable-dev-shm-usage")
+            chrome_options.add_argument("--no-sandbox")
+            driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), chrome_options=chrome_options)
 
         driver.get("https://hascyberpunkbeendelayedagain.com")
 
